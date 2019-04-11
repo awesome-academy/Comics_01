f7febd8db50b6eef49abf4415e66f0d8fb0442df
VanAnh <majanhqn@gmail.com>
2019-04-11 14:43:17 +0700

    add event for datail comic screen

---
+++ app/src/main/java/com/sun_asterisk/comics_01/utils/StringUtils.java
@@ -0,0 +1,19 @@
+package com.sun_asterisk.comics_01.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StringUtils {
+    public final static String DIVISION_CHARACTER = "T";
+    public final static String SEPARATE = ",\\n";
+
+    public static String formatDate(String date) {
+        int endIndex = date.indexOf(DIVISION_CHARACTER);
+        return date.substring(0, endIndex);
+    }
+
+    public static List<String> formatStrToStrUrls(String data) {
+        String[] arrayStrUrls = data.split(SEPARATE);
+        return Arrays.asList(arrayStrUrls);
+    }
+}
